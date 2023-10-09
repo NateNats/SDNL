f944434cf76a5851ce4eecad023335c159491cbf
Nicolaus Reva <nicolaussagraha14@gmail.com>
2023-10-09 22:48:02 +0700

    repairing bug from infix class (cek.data)

--- src/main/java/SDNL/TreeExpression/Infix.java
+++ src/main/java/SDNL/TreeExpression/Infix.java
@@ -1,18 +1,16 @@
 package SDNL.TreeExpression;
 
 public class Infix {
-    KonstantaInfix karInFix = new KonstantaInfix(); //variable karInFix referensi dari kelas KonstantaInfix
-    char[] ung;                                     //variable array char ung
-    StackTree opr;                                  //variable opr referensi dari keals StackTree
-    StackTree opd;                                  //variable opd referensi dari keals StackTree
+    KonstantaInfix karInFix = new KonstantaInfix();
+    char ung[];
+    StackTree opr;
+    StackTree opd;
 
-    //konstruktor infix dengan 1 parameter
     public Infix(char[] ung_in) {
         this.ung = ung_in;
     }
 
-    //method valensi untuk mendapatkan angka dari paramter input
-    public int valensi(char data_in) {
+    public int valensi (char data_in) {
         if (data_in == '^') {
             return 3;
         } else if ((data_in == '*') || (data_in == '/')) {
@@ -26,8 +24,6 @@ public class Infix {
         }
     }
 
-    //method gabung untuk menggabungkan rightNode dan leftNode dari stack opr dan opd
-    //sehingga terbentuk sebuah potongan node expression
     public TreeNode gabung() {
         TreeNode phn = opr.pop();
         phn.rightNode = opd.pop();
@@ -35,13 +31,12 @@ public class Infix {
         return phn;
     }
 
-    //method untuk membuat pohon expression
     public TreeNode buatPohon() {
         char kar;
         TreeNode treeKar;
         TreeNode cek;
-        StackTree opr = new StackTree(); // variable opr/operator dari StackTree
-        StackTree opd = new StackTree(); // variable opd/operand dari StackTree
+        opr = new StackTree();
+        opd = new StackTree();
 
         for (int i = 0; i < ung.length; i++) {
             kar = ung[i];
@@ -49,105 +44,91 @@ public class Infix {
             if (kar != ' ') {
                 treeKar = new TreeNode(kar);
                 if (kar == '(') {
-                    opr.push(treeKar); // Push '(' ke stack operator
+                    opr.push(treeKar);
                 } else if (kar == ')') {
-                    cek = opr.pop(); // Pop operator dari stack operator
+                    cek = opr.pop();
                     opr.push(cek);
 
-                    while (cek.data != '(') {   //loop while ketika data dari cek tidak sama dengan '('
-                        opd.push(gabung()); // Push hasil gabungan operand ke stack operand
-                        cek = opr.pop(); // Pop operator dari stack operator
+                    while (cek.data != '(') {
+                        opd.push(gabung());
+                        cek = opr.pop();
                         opr.push(cek);
                     }
 
-                    opr.pop(); // Pop tanda '(' dari stack operator
+                    opr.pop();
                 } else if (karInFix.isOperator()) {
-                    cek = opr.pop(); // Pop operator dari stack operator
+                    cek = opr.pop();
                     opr.push(cek);
 
-                    while ((cek != null) && (valensi(kar) <= valensi(cek.data))) {
-                        opd.push(gabung()); // Push hasil gabungan operand ke stack operand
-                        cek = opr.pop(); // Pop operator dari stack operator
+                    while((cek != null) && (valensi(kar) <= valensi(cek.data))) {
+                        opd.push(gabung());
+                        cek = opr.pop();
                         opr.push(cek);
                     }
-                    opr.push(treeKar); // Push operator ke stack operator
 
+                    opr.push(treeKar);
                 } else if (karInFix.isOperand()) {
-                    opd.push(treeKar); // Push operand ke stack operand
-
+                    opd.push(treeKar);
                 } else {
-                    return null; //jika if dan else tidak memenuhi, maka return null
+                    return null;
                 }
             }
         }
 
-        cek = opr.pop(); // Pop operator dari stack operator
+        cek = opr.pop();
         opr.push(cek);
 
-        while (cek != null) {
-            opd.push(gabung()); // Push hasil gabungan operand ke stack operand
+        while(cek != null) {
+            opd.push(gabung());
 
-            cek = opr.pop(); // Pop operator dari stack operator
+            cek = opr.pop();
             opr.push(cek);
         }
 
-        cek = opd.pop(); // Pop hasil akhir dari stack operand
-        opr = null; // set stack operator jadi null
-        opd = null; // set stack operand jadi null
-        return cek; // return cek
+        cek = opd.pop();
+        opr = null;
+        opd = null;
+        return cek;
+    }
+}
 
+class KonstantaInfix {
+
+    char operator[] = {'+', '-', '*', '/', '^'};
+    char operand[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    private char data;
+
+    public KonstantaInfix() {
     }
 
-    class KonstantaInfix {
+    public char getData() {
+        return data;
+    }
 
-        //char array operator
-        char[] operator = {'+', '-', '*', '/', '^'};
+    public void setData(char data) {
+        this.data = data;
+    }
 
-        //char array operand
-        char[] operand = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    public KonstantaInfix(char data_in) {
+        this.data = data_in;
+    }
 
-        //char  data
-        private char data;
-
-        //konstruktor KonstantaInfix
-        public KonstantaInfix() {
-        }
-
-        //method untuk mengambil data
-        public char getData() {
-            return data;
-        }
-
-        //method untuk memasang data
-        public void setData(char data) {
-            this.data = data;
-        }
-
-        //konstruktor KonstantaInfix
-        public KonstantaInfix(char data_in) {
-            this.data = data_in;
-        }
-
-
-        //method untuk mengecek apakah data termasuk operator atau bukan
-        public boolean isOperator() {
-            for (char c : operator) { //perulangan untuk mengambil data dari operator dan dimasukan ke c
-                if (data == c) {        //jika data sama dengan c, return true
-                    return true;
-                }
+    public boolean isOperator() {
+        for (int i = 0; i < operator.length; i++) {
+            if (data == operator[i]) {
+                return true;
             }
-            return false;               //return false jika perulangan tidak ada yang benar
         }
+        return false;
+    }
 
-        //method untuk mengecek apakah data termasuk operand atau bukan
-        public boolean isOperand() {
-            for (char c : operand) { //perulangan untuk mengambil data dari operand dan dimasukan ke c
-                if (data == c) {        //jika data sama dengan c, return true
-                    return true;
-                }
+    public boolean isOperand() {
+        for (int i = 0; i < operand.length; i++) {
+            if (data == operand[i]) {
+                return true;
             }
-            return false;               //return false jika perulangan tidak ada yang benar
         }
+        return false;
     }
 }
--- src/main/java/SDNL/TreeExpression/TreeTest.java
+++ src/main/java/SDNL/TreeExpression/TreeTest.java
@@ -2,8 +2,9 @@ package SDNL.TreeExpression;
 
 public class TreeTest {
     public static void main(String[] args) {
-        String s1 = "(A+B)";                                        //mendeklarasikan variable String s1 dengan value (A+B)
-
+//        String s1 = "(A+B)";                                        //mendeklarasikan variable String s1 dengan value (A+B)
+        String s1 = "(A+B)*((B-C)+D)";
+//        String s1 = "(A+B*C)^((P+Q)*R)";
         char[] notasi = new char[s1.length()];                      //mendeklarasikan array char notasi dengan panjang sesuai dengan string s1
         s1.getChars(0, s1.length(), notasi, 0);    //mengambil character dari string s1, mulai dari index sampai akhir,
                                                                     //lalu memasukan ke array notasi mulai dari index 0
@@ -13,8 +14,11 @@ public class TreeTest {
 
         tree.setRootTree(infix.buatPohon());                        //tree memanggil method setRootTree dengan parameter infix.buatPohon
 
-        System.out.println("\nInfix");
+        System.out.print("\nInfix: ");
         tree.inOrderTraversal();                                    //memanggil method inOrderTraversal dari tree
-        System.out.println(" ");
+        System.out.print("\nPrefix: ");
+        tree.preOrderTraversal();
+        System.out.print("\nPostfix: ");
+        tree.postOrderTraversal();
     }
 }
