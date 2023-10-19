5cea2a579242d0a720e6e2d9bf87a7099107d01b
Nicolaus Reva <nicolaussagraha14@gmail.com>
2023-10-19 22:22:01 +0700

    add new binary tree search

--- src/main/java/SDNL/Graph/Graph.java
+++ src/main/java/SDNL/Graph/Graph.java
@@ -1,5 +1,8 @@
 package SDNL.Graph;
 
+import com.sun.security.jgss.GSSUtil;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Arrays;
 
 public class Graph {
@@ -55,33 +58,57 @@ public class Graph {
     }
 
     public void show(){
-        System.out.print("|\t\t|\t");
-        for (int i = 0; i < adjancencyMatrix.length; i++) {
-            System.out.print(vertexList[i].getLabel());
+        StringBuilder sb = new StringBuilder();
 
-            if (i < adjancencyMatrix.length-1) {
-                System.out.print("\t|\t");
-            }
+        sb.append('|');
+        sb.append('\t');
+        sb.append('\t');
+        sb.append('|');
 
+        for (Vertex v : vertexList) {
 
+            sb.append('\t');
+            sb.append(v.getLabel());
+            sb.append('\t');
+            sb.append('|');
         }
-        System.out.print("\t|");
-        System.out.print("\n");
-        System.out.print("-".repeat(50));
-        System.out.print("\n");
-        for (int i = 0; i < adjancencyMatrix.length; i++) {
-            System.out.print("|\t");
-            System.out.print(vertexList[i].getLabel());
-            System.out.print("\t|\t");
 
-            for (int j = 0; j < adjancencyMatrix.length; j++) {
-                System.out.print(adjancencyMatrix[i][j]);
+        int length = (sb.length());
+        System.out.print("|");
+        System.out.print("-".repeat((length * 2)-1));
+        System.out.println("|");
 
-                System.out.print("\t|\t");
+        System.out.println(sb);
+        System.out.print("|");
+        printLine(length-3);
+        System.out.println("|");
+
+        for (int i = 0; i < vertexList.length; i++) {
+            System.out.print("|\t" + vertexList[i].getLabel() + "\t|");
+            for (int j = 0; j < vertexList.length; j++) {
+                System.out.print("\t" + adjancencyMatrix[i][j] + "\t|");
             }
-
-            System.out.print("\n");
+            System.out.print("\n|");
+            printLine(length-3);
+            System.out.println("|");
         }
+
+    }
+
+    public void printLine(int length) {
+        int value = length/3;
+
+        System.out.print("-".repeat(value));
+        System.out.print("+");
+        System.out.print("-".repeat(value));
+        System.out.print("+");
+        System.out.print("-".repeat(value));
+        System.out.print("+");
+        System.out.print("-".repeat(value));
+        System.out.print("+");
+        System.out.print("-".repeat(value));
+        System.out.print("+");
+        System.out.print("-".repeat(value));
     }
 
     @Override
